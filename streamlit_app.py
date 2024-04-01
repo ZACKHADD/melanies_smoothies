778bfdb9be8b59ff70d212802076537c0e568634
ZAKARIA HADDOUCH <59281379+ZACKHADD@users.noreply.github.com>
2024-04-01 22:22:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie!  :cup_with_straw:")
@@ -35,3 +36,7 @@ if ingredients:
         
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
 
+#API part
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#st.text(fruityvice_response)
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
